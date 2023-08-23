e8df8ccb0de3bf3441780b620bf2e60a00aad0fd
Voste126 <steveaustine126@gmail.com>
2023-08-23 14:53:10 +0300

    The anogram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
-# your code goes here!
+class Anagram:
+    def __init__(self,word):
+        self._word = word
+    
+    def match(self, word_list):
+        array_word =[]
+        #sorts the word casing to lower case 
+        sorted_word = sorted(self._word.lower())
+
+        for word in word_list:
+            #first test to make sure to similar words pass the second matching letter cases in both words pass
+            if word.lower() != self._word.lower()  and sorted(word.lower()) == sorted_word:
+                array_word.append(word)
+            
+        return array_word
+        
+
+        
